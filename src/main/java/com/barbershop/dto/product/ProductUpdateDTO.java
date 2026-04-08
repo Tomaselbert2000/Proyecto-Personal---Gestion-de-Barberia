3878db2483b53ca1cb952c26d1102f0f7d7001ba
Tomaselbert2000 <tomas@debian.tomas>
2026-04-08 00:10:05 -0300

    refactor: agrega mensajes descriptivos para annotations en la clase ProductUpdateDTO

--- src/main/java/com/barbershop/dto/product/ProductUpdateDTO.java
+++ src/main/java/com/barbershop/dto/product/ProductUpdateDTO.java
@@ -18,45 +18,45 @@ import static com.barbershop.validation.product.ProductValidatorConstants.*;
 @Builder
 public class ProductUpdateDTO implements ProductDTOCommonMethods{
 
-    @Size(min = MIN_NAME_LENGTH, max = MAX_NAME_LENGTH)
-    @Pattern(regexp = NAME_REGEX)
+    @Size(min = MIN_NAME_LENGTH, max = MAX_NAME_LENGTH, message = PRODUCT_NAME + INVALID_NAME_SIZE)
+    @Pattern(regexp = NAME_REGEX, message = PRODUCT_NAME + DOES_NOT_MATCH_REGEX)
     private String name;
 
-    @Size(max = MAX_OPTIONAL_DESCRIPTION_LENGTH)
+    @Size(max = MAX_OPTIONAL_DESCRIPTION_LENGTH, message = OPTIONAL_DESCRIPTION + OPTIONAL_TEXT_MAX_SIZE)
     private String optionalDescription;
 
-    @Size(min = MIN_NAME_LENGTH, max = MAX_NAME_LENGTH)
-    @Pattern(regexp = NAME_REGEX)
+    @Size(min = MIN_NAME_LENGTH, max = MAX_NAME_LENGTH, message = BRAND_NAME + INVALID_NAME_SIZE)
+    @Pattern(regexp = NAME_REGEX, message = BRAND_NAME + DOES_NOT_MATCH_REGEX)
     private String brandName;
 
     private ProductPresentationUnit presentationUnit;
 
-    @Positive
+    @Positive(message = PRESENTATION_SIZE + POSITIVE)
     private Integer presentationSize;
 
-    @Positive
+    @Positive(message = PRODUCT_COST + POSITIVE)
     private Double productCost;
 
-    @Positive
+    @Positive(message = PRODUCT_MIN_PRICE + POSITIVE)
     private Double minPrice;
 
-    @Positive
+    @Positive(message = PRODUCT_CURRENT_PRICE + POSITIVE)
     private Double currentPrice;
 
-    @Positive
+    @Positive(message = PRODUCT_WHOLE_SALE_PRICE + POSITIVE)
     private Double productWholeSalePrice;
 
-    @PositiveOrZero
+    @PositiveOrZero(message = PRODUCT_DISCOUNT_VALUE + HIGHER_OR_EQUALS_THAN_ZERO)
     @DecimalMin(value = MIN_DISCOUNT_VALUE)
     @DecimalMax(value = MAX_DISCOUNT_VALUE)
     private Double maxDiscountPercentage;
 
     private ProductCategory category;
 
-    @PositiveOrZero
+    @PositiveOrZero(message = PRODUCT_CURRENT_STOCK_LEVEL + HIGHER_OR_EQUALS_THAN_ZERO)
     private Integer currentStockLevel;
 
-    @PositiveOrZero
+    @PositiveOrZero(message = PRODUCT_SAFETY_STOCK_LEVEL + HIGHER_OR_EQUALS_THAN_ZERO)
     private Integer safetyStockLevel;
 
     private String imageFilePath;
