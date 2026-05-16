e39b4111cb6438b62af8502e9c389e1cf50f45e2
Tomaselbert2000 <tomas@debian.tomas>
2026-05-16 18:13:36 -0300

    feat: agrega clase para constantes referidas a íconos MD3

---
+++ src/main/java/com/barbershop/launcher/constants/ui/css_class/MaterialDesignIcon.java
@@ -0,0 +1,12 @@
+package com.barbershop.launcher.constants.ui.css_class;
+
+public class MaterialDesignIcon {
+
+    public static final String ADD_ALERT_ICON = "add-alert-icon";
+    public static final String PERSON_REMOVE = "person-remove-icon";
+    public static final String CANCEL_PERSON_ICON = "person-cancel-icon";
+    public static final String INFO_ICON = "info-icon";
+    public static final String ENTERPRISE_OFF_ICON = "enterprise-off-icon";
+    public static final String WARNING_ICON = "warning-icon";
+    public static final String DONE_ALL_ICON = "done-all-icon";
+}
