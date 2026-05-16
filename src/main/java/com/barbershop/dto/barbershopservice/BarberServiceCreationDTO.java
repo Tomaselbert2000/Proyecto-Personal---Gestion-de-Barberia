b9549992c12c9b24f2ae3a961f4d04e9b3a166c7
Tomaselbert2000 <tomas@debian.tomas>
2026-05-16 17:56:03 -0300

    fix: agrega mensajes informativos faltantes para anotaciones de BarberServiceCreationDTO.java

--- src/main/java/com/barbershop/dto/barbershopservice/BarberServiceCreationDTO.java
+++ src/main/java/com/barbershop/dto/barbershopservice/BarberServiceCreationDTO.java
@@ -4,6 +4,8 @@ import com.barbershop.enums.BarberServiceCategory;
 import jakarta.validation.constraints.*;
 import lombok.*;
 
+import static com.barbershop.launcher.constants.entity_constraints.BarberServiceConstraintViolationMessageSubject.*;
+import static com.barbershop.launcher.constants.entity_constraints.ConstraintViolationMessagePredicate.*;
 import static com.barbershop.utils.strings.RegexPatterns.NAME_REGEX;
 import static com.barbershop.validation.common.CommonConstants.*;
 
@@ -14,20 +16,20 @@ import static com.barbershop.validation.common.CommonConstants.*;
 @Builder
 public class BarberServiceCreationDTO {
 
-    @NotNull
-    @NotBlank
-    @Pattern(regexp = NAME_REGEX)
-    @Size(min = MIN_NAME_LENGTH, max = MAX_NAME_LENGTH)
+    @NotNull(message = BARBER_SERVICE_NAME + NOT_NULL)
+    @NotBlank(message = BARBER_SERVICE_NAME + NOT_BLANK)
+    @Pattern(regexp = NAME_REGEX, message = BARBER_SERVICE_NAME + DOES_NOT_MATCH_REGEX)
+    @Size(min = MIN_NAME_LENGTH, max = MAX_NAME_LENGTH, message = BARBER_SERVICE_NAME + INVALID_NAME_SIZE)
     private String name;
 
-    @NotNull
-    @Positive
+    @NotNull(message = BARBER_SERVICE_PRICE + NOT_NULL)
+    @Positive(message = BARBER_SERVICE_PRICE + POSITIVE)
     private Double price;
 
-    @NotNull
+    @NotNull(message = BARBER_SERVICE_CATEGORY + NOT_NULL)
     private BarberServiceCategory serviceCategory;
 
-    @NotNull
-    @Size(max = MAX_OPTIONAL_DESCRIPTION_LENGTH)
+    @NotNull(message = BARBER_SERVICE_INTERNAL_NOTES + NOT_NULL)
+    @Size(max = MAX_OPTIONAL_DESCRIPTION_LENGTH, message = BARBER_SERVICE_INTERNAL_NOTES + OPTIONAL_TEXT_MAX_SIZE)
     private String internalNotes;
 }
