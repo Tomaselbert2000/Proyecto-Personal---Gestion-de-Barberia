d57913f210f27d3ee1a25d5178ca37d0e44f4b52
Tomaselbert2000 <tomas@debian.tomas>
2026-05-26 20:31:13 -0300

    feat: agrega controlador para edición de clientes

---
+++ src/main/java/com/barbershop/launcher/controller/implementation/client/ClientEditionController.java
@@ -0,0 +1,67 @@
+package com.barbershop.launcher.controller.implementation.client;
+
+import com.barbershop.dto.client.ClientInfoDTO;
+import com.barbershop.launcher.controller.interfaces.EditionController;
+import com.barbershop.service.interfaces.ClientService;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXCheckbox;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ClientEditionController implements EditionController<ClientInfoDTO> {
+
+    private final ApplicationContext applicationContext;
+    private final ClientService clientService;
+
+    @FXML
+    private AnchorPane anchor_pane;
+
+    @FXML
+    private MFXButton
+            back_button,
+            add_phone_button,
+            cancel_button,
+            save_button;
+
+    @FXML
+    private MFXTextField
+            dni_field,
+            first_name_field,
+            last_name_field,
+            email_field,
+            optional_notes_field;
+
+    @FXML
+    private VBox phones_container;
+
+    @FXML
+    private MFXCheckbox client_active_checkbox;
+
+    @FXML
+    public void initialize() {
+
+
+    }
+
+    @Override
+    public void configureButtonActions(ClientInfoDTO infoDTO) {
+
+    }
+
+    @Override
+    public void resetForm(ClientInfoDTO infoDTO) {
+
+    }
+
+    @Override
+    public void configureButtonActions() {
+
+    }
+}
