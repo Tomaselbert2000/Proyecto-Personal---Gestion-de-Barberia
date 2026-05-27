d931949718c3d0a80c56c535c61601aa9c0665f5
Tomaselbert2000 <tomas@debian.tomas>
2026-05-26 23:00:08 -0300

    fix: agrega mensajes de violación de restricciones faltantes para ClientCreationDTO

--- src/main/java/com/barbershop/dto/client/ClientCreationDTO.java
+++ src/main/java/com/barbershop/dto/client/ClientCreationDTO.java
@@ -5,6 +5,8 @@ import lombok.*;
 
 import java.util.List;
 
+import static com.barbershop.launcher.constants.entity_constraints.ClientConstraintViolationMessageSubject.*;
+import static com.barbershop.launcher.constants.entity_constraints.ConstraintViolationMessagePredicate.*;
 import static com.barbershop.utils.strings.RegexPatterns.*;
 import static com.barbershop.validation.client.ClientValidatorConstants.*;
 import static com.barbershop.validation.common.CommonConstants.*;
@@ -14,31 +16,31 @@ import static com.barbershop.validation.common.CommonConstants.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class ClientCreationDTO implements ClientDTOCommonMethods{
+public class ClientCreationDTO implements ClientDTOCommonMethods {
 
-    @NotBlank
-    @Size(min = MIN_NATIONAL_ID_CARD_NUMBER_LENGTH, max = MAX_NATIONAL_ID_CARD_NUMBER_LENGTH)
-    @Pattern(regexp = NATIONAL_ID_CARD_NUMBER_REGEX)
+    @NotBlank(message = CLIENT_NATIONAL_ID_CARD_NUMBER + NOT_BLANK)
+    @Size(min = MIN_NATIONAL_ID_CARD_NUMBER_LENGTH, max = MAX_NATIONAL_ID_CARD_NUMBER_LENGTH, message = CLIENT_NATIONAL_ID_CARD_NUMBER + INVALID_NAME_SIZE)
+    @Pattern(regexp = NATIONAL_ID_CARD_NUMBER_REGEX, message = CLIENT_NATIONAL_ID_CARD_NUMBER + DOES_NOT_MATCH_NAME_REGEX)
     private String nationalIdentityCardNumber;
 
-    @NotBlank
-    @Size(min = MIN_NAME_LENGTH, max = MAX_NAME_LENGTH)
-    @Pattern(regexp = NAME_REGEX)
+    @NotBlank(message = CLIENT_FIRST_NAME + NOT_BLANK)
+    @Size(min = MIN_NAME_LENGTH, max = MAX_NAME_LENGTH, message = CLIENT_FIRST_NAME + INVALID_NAME_SIZE)
+    @Pattern(regexp = NAME_REGEX, message = CLIENT_FIRST_NAME + DOES_NOT_MATCH_NAME_REGEX)
     private String firstName;
 
-    @NotBlank
-    @Size(min = MIN_NAME_LENGTH, max = MAX_NAME_LENGTH)
-    @Pattern(regexp = NAME_REGEX)
+    @NotBlank(message = CLIENT_LAST_NAME + NOT_BLANK)
+    @Size(min = MIN_NAME_LENGTH, max = MAX_NAME_LENGTH, message = CLIENT_LAST_NAME + INVALID_NAME_SIZE)
+    @Pattern(regexp = NAME_REGEX, message = CLIENT_LAST_NAME + DOES_NOT_MATCH_NAME_REGEX)
     private String lastName;
 
-    @NotBlank
-    @Pattern(regexp = EMAIL_REGEX)
+    @NotBlank(message = CLIENT_EMAIL + NOT_BLANK)
+    @Pattern(regexp = EMAIL_REGEX, message = CLIENT_EMAIL + DOES_NOT_MATCH_EMAIL_REGEX)
     private String email;
 
     @NotEmpty
-    private List<@NotBlank @Size(min = MIN_PHONE_LENGTH, max = MAX_PHONE_LENGTH) @Pattern(regexp = PHONE_REGEX) String> phoneNumbersList;
+    private List<@NotBlank(message = CLIENT_PHONE + NOT_BLANK) @Size(min = MIN_PHONE_LENGTH, max = MAX_PHONE_LENGTH) @Pattern(regexp = PHONE_REGEX, message = CLIENT_PHONE + DOES_NOT_MATCH_PHONE_REGEX) String> phoneNumbersList;
 
     @NotNull
-    @Size(max = MAX_OPTIONAL_DESCRIPTION_LENGTH)
+    @Size(max = MAX_OPTIONAL_DESCRIPTION_LENGTH, message = CLIENT_OPTIONAL_NOTES + OPTIONAL_TEXT_MAX_SIZE)
     private String optionalNotes;
 }
