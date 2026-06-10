7c1af1569c0d4b7448b7e665e41558f2cf1a226d
Tomaselbert2000 <tomas@debian.tomas>
2026-06-09 21:30:23 -0300

    feat: agrega Mapper, repositorio y service para AppUser

---
+++ src/main/java/com/barbershop/repository/AppUserRepository.java
@@ -0,0 +1,8 @@
+package com.barbershop.repository;
+
+import com.barbershop.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+}
