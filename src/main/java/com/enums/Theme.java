fcdf75a4fc8f2a6d6ee7560f1cfd5a6683d8ea8b
Tomaselbert2000 <tomas@debian.tomas>
2026-07-24 03:24:55 -0300

    refactor: modifica Theme.java acorde al nuevo sistema de tokens de diseño

--- src/main/java/com/enums/Theme.java
+++ src/main/java/com/enums/Theme.java
@@ -2,22 +2,21 @@ package com.enums;
 
 import lombok.Getter;
 
-import static com.launcher.constants.ui.themes.ThemeFilePath.*;
+import static com.launcher.constants.ThemeFilePath.*;
 
 @Getter
 public enum Theme implements DescribableEnum {
 
     MD3_LIGHT("MD3 Claro", MD3_LIGHT_THEME_FILE_PATH),
-    MD3_DARK("MD3 Oscuro", MD3_DARK_DARK_THEME_FILE_PATH),
+    MD3_DARK("MD3 Oscuro", MD3_DARK_THEME_FILE_PATH),
     LA_TERCERA_LIGHT("La Tercera - Claro", LA_TERCERA_LIGHT_THEME_FILE_PATH),
     LA_TERCERA_DARK("La Tercera - Oscuro", LA_TERCERA_DARK_THEME_FILE_PATH),
-    CUSTOM("Personalizado", "custom.css");
+    CUSTOM("Personalizado", "/styles/tokens/custom.css");
 
     private final String displayName;
     private final String themeFilePath;
 
     Theme(String displayName, String themeFilePath) {
-
         this.displayName = displayName;
         this.themeFilePath = themeFilePath;
     }
