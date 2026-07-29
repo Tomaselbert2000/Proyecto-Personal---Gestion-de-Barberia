e61bad18ec3d7c7ee92ab5c97520cc3e2ddf8341
Tomaselbert2000 <tomas@debian.tomas>
2026-07-29 15:58:00 -0300

    refactor: elimina interfaz innecesesaria en capa de repositorio

--- src/main/java/com/repository/ServiceRecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.repository;
-
-import com.model.ServiceRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceRecordRepository extends JpaRepository<ServiceRecord, Long> {
-}
