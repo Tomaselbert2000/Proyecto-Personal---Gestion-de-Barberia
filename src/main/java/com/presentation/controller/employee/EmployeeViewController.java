34cfb1a8ef8dfd03cc10355ac8fa75c504515145
Tomaselbert2000 <tomas@debian.tomas>
2026-08-07 12:48:02 -0300

    fix: corrige ruta de vista en EmployeeViewController

--- src/main/java/com/presentation/controller/employee/EmployeeViewController.java
+++ src/main/java/com/presentation/controller/employee/EmployeeViewController.java
@@ -143,7 +143,7 @@ public class EmployeeViewController {
         loadItemsOnController(
                 employeeList,
                 employee_list_container,
-                APPOINTMENT_ITEM_VIEW_PATH,
+                EMPLOYEE_ITEM_VIEW_PATH,
                 EMPTY_EMPLOYEE_LIST_MESSAGE,
                 EMPLOYEE_ITEM_VIEW_LOADING_FAILED,
                 itemController -> {
