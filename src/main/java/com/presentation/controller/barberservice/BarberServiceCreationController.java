c426b587fec871238ad68205817e22ef2adfb90f
Tomaselbert2000 <tomas@debian.tomas>
2026-08-13 16:05:14 -0300

    refactor: extrae lógica de CRUD a BaseCrudFormController para controladores de servicios en catálogo

--- src/main/java/com/presentation/controller/barberservice/BarberServiceCreationController.java
+++ src/main/java/com/presentation/controller/barberservice/BarberServiceCreationController.java
@@ -1,113 +1,135 @@
 package com.presentation.controller.barberservice;
 
 import com.dto.barberservice.BarberServiceCreationDTO;
+import com.dto.barberservice.BarberServiceInfoDTO;
 import com.enums.BarberServiceCategory;
-import com.enums.ToastNotificationType;
-import com.enums.ViewRedirection;
+import com.presentation.controller.BaseCrudFormController;
 import com.service.interfaces.BarberserviceService;
 import io.github.palexdev.materialfx.controls.MFXButton;
-import jakarta.validation.ConstraintViolationException;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
 
+import static com.enums.ViewRedirection.BARBER_SERVICES;
 import static com.presentation.constants.PromptTexts.BarberServicePromptText.*;
-import static com.presentation.constants.StringResource.ConfirmationDialog.CONFIRM_BUTTON_TEXT;
 import static com.presentation.constants.StringResource.ToastNotificationMessage.BARBER_SERVICE_CREATION_TOAST_NOTIFICATION_MESSAGE;
 import static com.presentation.constants.StringResource.ValidationErrorMessage.BARBER_SERVICE_CREATION_VALIDATION_FAILED;
-import static com.presentation.constants.StringResource.ValidationErrorMessage.VALIDATION_ERROR_TITLE;
 import static com.presentation.support.control.ComboBoxHelper.loadEnumsOnComboBox;
 import static com.presentation.support.control.ComboBoxHelper.removeFirstItemFromComboBox;
 import static com.presentation.support.control.UIBasicComponents.*;
-import static com.presentation.support.control.ValidationFormatter.getConstraintViolationsList;
 import static com.presentation.support.control.ValidationFormatter.setStringConverter;
-import static com.presentation.support.dialog.PopUpWindowHelper.showWindowAlert;
 import static com.presentation.support.format.NumberParser.parsePrice;
-import static com.presentation.support.notification.ToastNotificationHelper.showToastNotification;
-import static com.presentation.support.view.ContainerManager.getCurrentWindow;
 import static com.presentation.support.view.ViewRedirectionHelper.redirectToView;
 
 @Component
-@RequiredArgsConstructor
-public class BarberServiceCreationController {
+public class BarberServiceCreationController extends BaseCrudFormController<BarberServiceCreationDTO, BarberServiceInfoDTO> {
 
     private final BarberserviceService barberserviceService;
-    private final ApplicationContext applicationContext;
+
+    public BarberServiceCreationController(ApplicationContext applicationContext, BarberserviceService barberserviceService) {
+
+        super(applicationContext);
+        this.barberserviceService = barberserviceService;
+    }
 
     @FXML
     private AnchorPane anchorPane;
 
     @FXML
-    private TextField serviceNameField;
-    @FXML
-    private TextField priceField;
-    @FXML
-    private TextField internalNotesField;
+    private TextField
+            serviceNameField,
+            priceField,
+            internalNotesField;
 
     @FXML
     private ComboBox<BarberServiceCategory> categoryComboBox;
 
     @FXML
-    private MFXButton backButton;
-    @FXML
-    private MFXButton cleanFieldsButton;
-    @FXML
-    private MFXButton saveButton;
+    private MFXButton
+            backButton,
+            cleanFieldsButton,
+            saveButton;
 
     @FXML
     public void initialize() {
+
         configureUI();
         configureButtonActions();
     }
 
-    /**
-     * Configura la interfaz de usuario, incluyendo el formato de texto para los campos de precio y la carga de categorías de servicios.
-     */
     private void configureUI() {
+
         configureDecimalTextfieldRestrictions(priceField);
+
         loadEnumsOnComboBox(categoryComboBox, BarberServiceCategory.values());
+
         setStringConverter(categoryComboBox, BarberServiceCategory.TODOS);
+
         removeFirstItemFromComboBox(categoryComboBox);
+
         configurePromptTexts();
     }
 
-    /**
-     * Registra un nuevo servicio de barbero.
-     */
-    private void registerNewBarberService() {
-        try {
-            String serviceName = serviceNameField.getText();
-            Double price = parsePrice(priceField.getText());
-            BarberServiceCategory category = categoryComboBox.getValue();
-            String internalNotes = internalNotesField.getText();
-            BarberServiceCreationDTO creationDTO = buildDTOFromAttributes(serviceName, price, category, internalNotes);
-            barberserviceService.registerNewBarberService(creationDTO);
-            showToastNotification(anchorPane, applicationContext, BARBER_SERVICE_CREATION_TOAST_NOTIFICATION_MESSAGE, ToastNotificationType.SUCCESSFUL);
-        } catch (ConstraintViolationException exception) {
-            String errorMessage = getConstraintViolationsList(exception);
-            showWindowAlert(VALIDATION_ERROR_TITLE, BARBER_SERVICE_CREATION_VALIDATION_FAILED, errorMessage, Alert.AlertType.ERROR, CONFIRM_BUTTON_TEXT, getCurrentWindow(anchorPane));
-        }
+    private void configurePromptTexts() {
+
+        Map<TextField, String> map = Map.of(
+                serviceNameField, BARBER_SERVICE_NAME,
+                priceField, BARBER_SERVICE_PRICE,
+                internalNotesField, BARBER_SERVICE_INTERNAL_NOTES
+        );
+
+        setPromptTextOnMap(map);
     }
 
-    /**
-     * Construye un DTO de creación de servicio de barbero con los datos proporcionados.
-     *
-     * @param serviceName   El nombre del servicio de barbero.
-     * @param price         El precio del servicio de barbero.
-     * @param category      La categoría del servicio de barbero.
-     * @param internalNotes Las notas internas del servicio de barbero.
-     * @return Un DTO de creación de servicio de barbero con los datos proporcionados.
-     */
-    private BarberServiceCreationDTO buildDTOFromAttributes(String serviceName, Double price, BarberServiceCategory category, String internalNotes) {
+    private void configureButtonActions() {
+
+        Map<Button, Runnable> map = Map.of(
+                backButton, () -> redirectToView(BARBER_SERVICES, getAnchorPane(), getApplicationContext()),
+                cleanFieldsButton, this::resetForm,
+                saveButton, this::saveEntity
+        );
+
+        configureRunnableMaps(map);
+    }
+
+    @Override
+    protected AnchorPane getAnchorPane() {
+
+        return anchorPane;
+    }
+
+    @Override
+    protected void persistEntity(BarberServiceCreationDTO dto) {
+
+        barberserviceService.registerNewBarberService(dto);
+    }
+
+    @Override
+    protected String getSuccessMessage() {
+
+        return BARBER_SERVICE_CREATION_TOAST_NOTIFICATION_MESSAGE;
+    }
+
+    @Override
+    protected String getErrorMessage() {
+        return BARBER_SERVICE_CREATION_VALIDATION_FAILED;
+    }
+
+    @Override
+    protected BarberServiceCreationDTO buildDTO() {
+
+        String serviceName = serviceNameField.getText();
+        Double price = parsePrice(priceField.getText());
+        BarberServiceCategory category = categoryComboBox.getValue();
+        String internalNotes = internalNotesField.getText();
+
         return BarberServiceCreationDTO.builder()
                 .name(serviceName).
                 price(price)
@@ -116,25 +138,9 @@ public class BarberServiceCreationController {
                 .build();
     }
 
-    private void configurePromptTexts() {
-        Map<TextField, String> map = Map.of(
-                serviceNameField, BARBER_SERVICE_NAME,
-                priceField, BARBER_SERVICE_PRICE,
-                internalNotesField, BARBER_SERVICE_INTERNAL_NOTES
-        );
-        setPromptTextOnMap(map);
-    }
+    @Override
+    protected void resetForm() {
 
-    private void resetForm() {
         cleanTextfields(List.of(serviceNameField, priceField, internalNotesField));
-    }
-
-    private void configureButtonActions() {
-        Map<Button, Runnable> map = Map.of(
-                backButton, () -> redirectToView(ViewRedirection.BARBER_SERVICES, anchorPane, applicationContext),
-                cleanFieldsButton, this::resetForm,
-                saveButton, this::registerNewBarberService
-        );
-        configureRunnableMaps(map);
     }
 }
--- src/main/java/com/presentation/controller/barberservice/BarberServiceEditionController.java
+++ src/main/java/com/presentation/controller/barberservice/BarberServiceEditionController.java
@@ -3,43 +3,40 @@ package com.presentation.controller.barberservice;
 import com.dto.barberservice.BarberServiceInfoDTO;
 import com.dto.barberservice.BarberServiceUpdateDTO;
 import com.enums.BarberServiceCategory;
-import com.enums.ToastNotificationType;
-import com.enums.ViewRedirection;
+import com.presentation.controller.BaseCrudFormController;
 import com.service.interfaces.BarberserviceService;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.Map;
 
+import static com.enums.ViewRedirection.BARBER_SERVICES;
 import static com.presentation.constants.StringResource.ToastNotificationMessage.BARBER_SERVICE_UPDATE_TOAST_NOTIFICATION_MESSAGE;
+import static com.presentation.constants.StringResource.ValidationErrorMessage.BARBER_SERVICE_UPDATE_VALIDATION_FAILED;
 import static com.presentation.support.control.ComboBoxHelper.loadEnumsOnComboBox;
 import static com.presentation.support.control.ComboBoxHelper.removeFirstItemFromComboBox;
 import static com.presentation.support.control.UIBasicComponents.*;
 import static com.presentation.support.format.NumberParser.parsePrice;
-import static com.presentation.support.notification.ToastNotificationHelper.showExceptionErrorMessage;
-import static com.presentation.support.notification.ToastNotificationHelper.showToastNotification;
 import static com.presentation.support.view.ViewRedirectionHelper.redirectToView;
 
-/**
- * Controlador para la edición de servicios de barbero.
- * Se encarga de gestionar la interfaz de usuario y las operaciones relacionadas con la edición de servicios de barbero.
- */
 @Component
-@RequiredArgsConstructor
-public class BarberServiceEditionController {
+public class BarberServiceEditionController extends BaseCrudFormController<BarberServiceUpdateDTO, BarberServiceInfoDTO> {
 
     private final BarberserviceService barberserviceService;
-    private final ApplicationContext applicationContext;
+
+    public BarberServiceEditionController(ApplicationContext applicationContext, BarberserviceService barberserviceService) {
+
+        super(applicationContext);
+        this.barberserviceService = barberserviceService;
+    }
+
+    private BarberServiceInfoDTO internalReferenceToInfoDTO;
 
     @FXML
     private TextField
@@ -61,77 +58,81 @@ public class BarberServiceEditionController {
     private ComboBox<BarberServiceCategory> categoryComboBox;
 
     @FXML
-    private VBox errorMessageContainer;
-
-    @FXML
-    private Label errorMessageLabel;
-
-    /**
-     * Inicializa el controlador con los datos del servicio de barbero a editar.
-     *
-     * @param infoDTO Datos del servicio de barbero a editar.
-     */
-    @FXML
     public void initialize(BarberServiceInfoDTO infoDTO) {
+
+        internalReferenceToInfoDTO = infoDTO;
+
         loadServiceDataForEdition(infoDTO);
         configureUI();
-        configureButtonActions(infoDTO);
+        configureButtonActions();
     }
 
-    /**
-     * Configura la interfaz de usuario, cargando los valores y configurando las restricciones.
-     */
     private void configureUI() {
+
         loadEnumsOnComboBox(categoryComboBox, BarberServiceCategory.values());
+
         removeFirstItemFromComboBox(categoryComboBox);
+
         configureDecimalTextfieldRestrictions(priceField);
     }
 
-    /**
-     * Carga los datos del servicio de barbero en los campos correspondientes para su edición.
-     *
-     * @param infoDTO Datos del servicio de barbero a editar.
-     */
     private void loadServiceDataForEdition(BarberServiceInfoDTO infoDTO) {
+
         Map<TextField, String> map = Map.of(
                 serviceIdField, infoDTO.getBarberServiceId().toString(),
                 serviceNameField, infoDTO.getName(),
                 priceField, infoDTO.getPrice().toString(),
                 internalNotesField, infoDTO.getInternalNotes()
         );
+
+        categoryComboBox.setValue(infoDTO.getCategory());
+
         setTextsOnTextfieldMap(map);
     }
 
-    /**
-     * Actualiza el servicio de barbero en la base de datos.
-     *
-     * @throws BlankBarberServicePriceException Si el precio del servicio está vacío.
-     */
-    private void updateBarberService() {
-        try {
-            Long id = Long.valueOf(serviceIdField.getText());
-            String newName = serviceNameField.getText();
-            Double newPrice = parsePrice(priceField.getText());
-            BarberServiceCategory newCategory = categoryComboBox.getValue();
-            String newInternalNotes = internalNotesField.getText();
-            BarberServiceUpdateDTO updateDTO = buildDTOFromAttributes(newName, newPrice, newCategory, newInternalNotes);
-            barberserviceService.updateService(id, updateDTO);
-            showToastNotification(anchorPane, applicationContext, BARBER_SERVICE_UPDATE_TOAST_NOTIFICATION_MESSAGE, ToastNotificationType.SUCCESSFUL);
-        } catch (RuntimeException exception) {
-            showExceptionErrorMessage(exception, errorMessageLabel, errorMessageContainer);
-        }
+    private void configureButtonActions() {
+
+        Map<Button, Runnable> map = Map.of(
+                backButton, () -> redirectToView(BARBER_SERVICES, anchorPane, getApplicationContext()),
+                restoreValuesButton, this::resetForm,
+                updateButton, this::saveEntity
+        );
+
+        configureRunnableMaps(map);
     }
 
-    /**
-     * Construye un DTO de actualización a partir de los atributos proporcionados.
-     *
-     * @param newName          Nombre del servicio.
-     * @param newPrice         Precio del servicio.
-     * @param newCategory      Categoría del servicio.
-     * @param newInternalNotes Notas internas del servicio.
-     * @return DTO de actualización del servicio.
-     */
-    private BarberServiceUpdateDTO buildDTOFromAttributes(String newName, Double newPrice, BarberServiceCategory newCategory, String newInternalNotes) {
+    @Override
+    protected AnchorPane getAnchorPane() {
+
+        return anchorPane;
+    }
+
+    @Override
+    protected void persistEntity(BarberServiceUpdateDTO dto) {
+
+        barberserviceService.updateService(internalReferenceToInfoDTO.getBarberServiceId(), dto);
+    }
+
+    @Override
+    protected String getSuccessMessage() {
+
+        return BARBER_SERVICE_UPDATE_TOAST_NOTIFICATION_MESSAGE;
+    }
+
+    @Override
+    protected String getErrorMessage() {
+
+        return BARBER_SERVICE_UPDATE_VALIDATION_FAILED;
+    }
+
+    @Override
+    protected BarberServiceUpdateDTO buildDTO() {
+
+        String newName = serviceNameField.getText();
+        Double newPrice = parsePrice(priceField.getText());
+        BarberServiceCategory newCategory = categoryComboBox.getValue();
+        String newInternalNotes = internalNotesField.getText();
+
         return BarberServiceUpdateDTO.builder()
                 .name(newName)
                 .price(newPrice)
@@ -140,29 +141,9 @@ public class BarberServiceEditionController {
                 .build();
     }
 
-    /**
-     * Configura las acciones de los botones en la interfaz de usuario.
-     *
-     * @param infoDTO Datos del servicio de barbero a editar.
-     */
-    private void configureButtonActions(BarberServiceInfoDTO infoDTO) {
-        Map<Button, Runnable> map = Map.of(
-                backButton, () -> redirectToView(ViewRedirection.BARBER_SERVICES, anchorPane, applicationContext),
-                restoreValuesButton, () -> resetForm(infoDTO),
-                updateButton, this::updateBarberService
-        );
-        configureRunnableMaps(map);
-    }
+    @Override
+    protected void resetForm() {
 
-    /**
-     * Restablece el formulario con los datos del servicio de barbero original.
-     *
-     * @param infoDTO Datos del servicio de barbero a editar.
-     */
-    private void resetForm(BarberServiceInfoDTO infoDTO) {
-        cleanTextfields(List.of(serviceNameField, priceField, internalNotesField));
-        loadServiceDataForEdition(infoDTO);
-        configureUI();
+        loadServiceDataForEdition(internalReferenceToInfoDTO);
     }
-
 }
