9cab8395418e4ef498c7a6f62be5a937d5d4e1f2
Tomaselbert2000 <tomas@debian.tomas>
2026-08-19 16:31:29 -0300

    feat: agrega Bean de HostServices para integración con JavaFX

--- src/main/java/com/BarberiaNuevoApplication.java
+++ src/main/java/com/BarberiaNuevoApplication.java
@@ -2,7 +2,9 @@ package com;
 
 import com.presentation.bootstrap.BarberiaJavaFxApplication;
 import javafx.application.Application;
+import javafx.application.HostServices;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -12,5 +14,11 @@ public class BarberiaNuevoApplication {
     static void main(String[] args) {
 
         Application.launch(BarberiaJavaFxApplication.class, args);
+    }
+
+    @Bean
+    HostServices hostServices(){
+
+        return BarberiaJavaFxApplication.getGlobalHostServices();
     }
 }
