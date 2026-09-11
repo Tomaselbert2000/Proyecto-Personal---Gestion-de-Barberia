6b3434e6d3b3894acb79cbb9d0b989661f272a9c
Tomaselbert2000 <tomas@debian.tomas>
2026-09-11 16:35:35 -0300

    refactor: agrega sobrecarga en método para formateo de Strings de precios

--- src/main/java/com/presentation/support/format/PriceFormatter.java
+++ src/main/java/com/presentation/support/format/PriceFormatter.java
@@ -1,11 +1,18 @@
 package com.presentation.support.format;
 
+import java.math.BigDecimal;
+
 import static com.presentation.constants.StringResource.DisplayString.CURRENCY_STRING_ARG;
 import static com.presentation.constants.StringResource.StringFormat.PRICE_FORMAT;
 
 public class PriceFormatter {
 
     public static String format(Double price) {
+
+        return CURRENCY_STRING_ARG + String.format(PRICE_FORMAT, price);
+    }
+
+    public static String format(BigDecimal price){
 
         return CURRENCY_STRING_ARG + String.format(PRICE_FORMAT, price);
     }
