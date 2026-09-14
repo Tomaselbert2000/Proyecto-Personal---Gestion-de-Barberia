aaaa14ea456a6b0880d73a2d4f110493c4f97577
Tomaselbert2000 <tomas@debian.tomas>
2026-09-13 23:05:30 -0300

    feat: agrega clase para constantes de URLs

---
+++ src/main/java/com/presentation/constants/HtmlTemplatePath.java
@@ -0,0 +1,9 @@
+package com.presentation.constants;
+
+public final class HtmlTemplatePath {
+
+    private HtmlTemplatePath() {
+    }
+
+    public static final String LOGIN_HTML_PATH = "login";
+}
