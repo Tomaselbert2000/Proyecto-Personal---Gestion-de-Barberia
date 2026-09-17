c974027e1d46f5d73a65bebee52d8ebafad5272e
Tomaselbert2000 <tomas@debian.tomas>
2026-09-17 12:34:22 -0300

    refactor: agrega constante de ruta para el dashboard

--- src/main/java/com/presentation/constants/HtmlTemplatePath.java
+++ src/main/java/com/presentation/constants/HtmlTemplatePath.java
@@ -8,4 +8,5 @@ public final class HtmlTemplatePath {
     public static final String LOGIN_HTML_PATH = "login";
     public static final String REGISTER_HTML_PATH = "register";
     public static final String REDIRECT_LOGIN = "redirect:/" + LOGIN_HTML_PATH;
+    public static final String DASHBOARD_HTML_PATH = "dashboard";
 }
